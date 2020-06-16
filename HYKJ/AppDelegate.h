985256b6ba0539051284e250732cb85dba7b9c8d
guanqing123 <guanqinghz@163.com>
2020-06-16 12:46:05 +0800

    Initial Commit

---
+++ HYKJ/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HYKJ
+//
+//  Created by information on 2020/6/16.
+//  Copyright © 2020 hongyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ HYKJ/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  HYKJ
+//
+//  Created by information on 2020/6/16.
+//  Copyright © 2020 hongyan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
